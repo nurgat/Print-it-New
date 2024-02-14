e938d7e259eb6f92a58c2743d6e3a3ceab02462f
nurgat <142540895+nurgat@users.noreply.github.com>
2024-02-14 13:07:32 +0400

    Réalidsation de l'étape 3 : Ajout des bullets points au slide avec le 1er point selectionné

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,70 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-// ETAPE 1: Récuperer les éléments html, ajouter un addEventListener avec un consol.log
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
 
-let arrow_left = document.querySelector(".arrow_left");
+let diapoEnCours = 0
+	
+// ETAPE 2: Récuperer les éléments html, ajouter un addEventListener avec un consol.log
+
+const arrow_left = document.querySelector(".arrow_left");
 arrow_left.addEventListener("click", function () {
 	console.log("Clique sur la flèche gauche")
 });
 
-let arrow_right = document.querySelector(".arrow_right");
+const arrow_right = document.querySelector(".arrow_right");
 arrow_right.addEventListener("click", function () {
 	console.log("Clique sur la flèche droite")
 });
+
+// ETAPE 3: Ajouter les bullets-points au slider
+
+function ajouterBulletsPoints() {
+    const dotsContainer = document.querySelector('.dots');
+    for (let index = 0; index < slides.length; index++) {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        if (index === diapoEnCours) {
+            dot.classList.add('dot_selected');
+        }
+        dot.addEventListener('click', () => {
+            diapoEnCours = index;
+            changeSlideTo(diapoEnCours); // Assurez-vous que cette fonction est définie ailleurs
+        });
+        dotsContainer.appendChild(dot);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+	
